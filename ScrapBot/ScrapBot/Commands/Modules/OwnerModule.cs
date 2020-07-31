f887d33179dd4a4eb785b5d20a215ec142f61c17
Playwo <eliaswolf2001@t-online.de>
2020-07-31 22:28:02 +0200

    Add ban fun command

---
+++ ScrapBot/ScrapBot/Commands/Modules/OwnerModule.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Qmmands;
+using ScrapBot.Utils;
+
+namespace ScrapBot.Commands.Modules
+{
+    [RequireOwner]
+    public class OwnerModule : ScrapModule
+    {
+        [Command("ban")]
+        [RequireOwner]
+        public async Task FakeBanAsync(SocketUser user)
+        {
+            var embed = new EmbedBuilder()
+                .WithColor(EmbedColor.Success)
+                .WithTitle("Success")
+                .WithDescription($"{user} has been banned!\n" +
+                $"Thanks for making this discord server a little bit safer :)")
+                .Build();
+
+            await ReplyAsync(embed: embed);
+        }
+    }
+}
